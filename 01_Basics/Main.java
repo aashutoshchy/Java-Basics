87193f4ade6acabdac2ebca401dbe053ac84663a
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-03-01 12:47:20 +0545

    Add Main.java with initial Java program

--- 01_Basics/Main.java
+++ 01_Basics/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        // This is my first Java Program
+
+        /*
+         * This is a
+         * Multiline comment
+         */
+
         System.out.println("Hello world");
         System.out.println("First Java Code");
+
     }
 }
