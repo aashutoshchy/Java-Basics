9d3a00a4b0ec2caf95a5fc0d1bd573d42b9beca2
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-03-05 13:48:42 +0545

    Refactor data type examples: move primitive and reference classes to new directory structure and add arithmetic operator examples

---
+++ 05_arithmeticOperator/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int x = 6;
+        int y = 7;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x + y;
+        z = x % y; // Modulus Operator // gives remainder
+
+        // Assignment Operator
+        z = 10;
+        z += 5;
+        System.out.println(z);
+
+        // Increment and Decrement Operators
+        z++;
+        System.out.print(z);
+
+        z--;
+        System.out.print(z);
+
+        // Order of Operations [P-E-M-D-A-S]
+    }
+}
