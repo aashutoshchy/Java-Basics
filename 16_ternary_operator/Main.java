ed3984a0cc20d2efb6fa08c9d7762de351d7ca44
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-03-11 08:55:07 +0545

    Add weight conversion program with user input options to demonstrate Ternary Operator

---
+++ 16_ternary_operator/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ternary operator examples
+        // int age = 18;
+        // String isAdult = age > 18 ? "Adult" : "Not Adult";
+        // System.out.println(isAdult);
+
+        // int num = 5;
+        // String evenOrOdd = (num % 2 == 0) ? "Even" : "Odd";
+        // System.out.println(evenOrOdd);
+
+        int hours = 13;
+        String timeOfDay = (hours < 12) ? "A.M" : "P.M";
+
+        System.out.println(timeOfDay);
+    }
+}
