fff9a7ce919512480224c8091bf47461df6e5403
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-04-01 12:02:20 +0545

    Add user input handling and array demonstration in Main class

---
+++ 33_search_an_array/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 9, 2, 8, 3, 5, 4 };
+        int target = 2;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (target == numbers[i]) {
+                System.out.println("Elements found at index: " + i);
+            }
+        }
+    }
+}
