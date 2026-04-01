b351146947d0443a1aa11fdf6b885d5e64ec78d8
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-04-01 11:29:39 +0545

    Array in Java

---
+++ 31_Array/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = { "Mango", "Apple", "Banana", "Grapes" };
+
+        // Printing Array using for loop.
+        // for (int i = 0; i < fruits.length; i++) {
+        // System.out.println(fruits[i]);
+        // }
+
+        // Array sort
+        Arrays.sort(fruits);
+
+        // Using for each loop//enhanced for loop
+        // for (String fruit : fruits) {
+        // System.out.println(fruit);
+        // }
+
+    }
+}
