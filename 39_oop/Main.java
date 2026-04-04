485b94286743c60ec1e6393696b7f9babb4d5b50
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-04-04 17:32:30 +0545

    Add Car class and Main class for car operations

---
+++ 39_oop/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        // car.isRunning = true;
+
+        // System.out.println(car.make);
+        // System.out.println(car.model);
+        // System.out.println(car.isRunning);
+
+        Car.start();
+        Car.stop();
+    }
+}
